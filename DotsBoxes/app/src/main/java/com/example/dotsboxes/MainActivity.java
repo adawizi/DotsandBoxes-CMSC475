9151947f3a2c2887fef66076aacbfa2dfd7f7cde
adawizi <adawizi@vcu.edu>
2022-02-21 20:12:04 -0500

    Recreated Game
    The entire game grid was remade. Instead of using a grid object, a bunch of images are used. Each link can now be clicked and you are now able to identify what is clicked unlike before.

---
+++ DotsBoxes/app/src/main/java/com/example/dotsboxes/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.dotsboxes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView link1;
+    ImageView link2;
+    ImageView link3;
+    ImageView link4;
+    ImageView link5;
+    ImageView link6;
+    ImageView link7;
+    ImageView link8;
+    ImageView link9;
+    ImageView link10;
+    ImageView link11;
+    ImageView link12;
+    ImageView link13;
+    ImageView link14;
+    ImageView link15;
+    ImageView link16;
+    ImageView link17;
+    ImageView link18;
+    ImageView link19;
+    ImageView link20;
+    ImageView link21;
+    ImageView link22;
+    ImageView link23;
+    ImageView link24;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        link1 = findViewById(R.id.link1);
+        link1.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link2 = findViewById(R.id.link2);
+        link2.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link3 = findViewById(R.id.link3);
+        link3.setOnClickListener(v -> System.out.println("It worked!"));
+
+
+        link4 = findViewById(R.id.link4);
+        link4.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link5 = findViewById(R.id.link5);
+        link5.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link6 = findViewById(R.id.link6);
+        link6.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link7 = findViewById(R.id.link7);
+        link7.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link8 = findViewById(R.id.link8);
+        link8.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link9 = findViewById(R.id.link9);
+        link9.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link10 = findViewById(R.id.link10);
+        link10.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link11 = findViewById(R.id.link11);
+        link11.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link12 = findViewById(R.id.link12);
+        link12.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link13 = findViewById(R.id.link13);
+        link13.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link14 = findViewById(R.id.link14);
+        link14.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link15 = findViewById(R.id.link15);
+        link15.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link16 = findViewById(R.id.link16);
+        link16.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link17 = findViewById(R.id.link17);
+        link17.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link18 = findViewById(R.id.link18);
+        link18.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link19 = findViewById(R.id.link19);
+        link19.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link20 = findViewById(R.id.link20);
+        link20.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link21 = findViewById(R.id.link21);
+        link21.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link22 = findViewById(R.id.link22);
+        link22.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link23 = findViewById(R.id.link23);
+        link23.setOnClickListener(v -> System.out.println("It worked!"));
+
+        link24 = findViewById(R.id.link24);
+        link24.setOnClickListener(v -> System.out.println("It worked!"));
+
+    }
+
+}
