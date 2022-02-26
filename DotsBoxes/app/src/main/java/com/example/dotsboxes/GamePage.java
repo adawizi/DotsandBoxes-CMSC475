13ac7b0e3b6534ab856675fba7f4d4a9f017d094
cantillond <cantillond@vcu.edu>
2022-02-25 21:44:16 -0500

    add top bar to show player, scores, and turn

--- DotsBoxes/app/src/main/java/com/example/dotsboxes/GamePage.java
+++ DotsBoxes/app/src/main/java/com/example/dotsboxes/GamePage.java
@@ -1,8 +1,11 @@
 package com.example.dotsboxes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -15,7 +18,6 @@ import java.util.Arrays;
 public class GamePage extends AppCompatActivity {
 
     boolean isFilled = false;
-
     ImageView[] grid = new ImageView[24];
     TextView[] filled = new TextView[9];
     ArrayList<Integer> horizontal = new ArrayList<>(Arrays.asList(1, 2, 3, 8, 9, 10, 15, 16, 17, 22, 23, 24));
@@ -23,6 +25,10 @@ public class GamePage extends AppCompatActivity {
     ArrayList<Integer> rightEdges = new ArrayList<>(Arrays.asList(7, 14, 21));
     ArrayList<Integer> topEdges = new ArrayList<>(Arrays.asList(1, 2, 3));
     ArrayList<Integer> bottomEdges = new ArrayList<>(Arrays.asList(22, 23, 24));
+    final int[] scores = {0, 0};
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +39,14 @@ public class GamePage extends AppCompatActivity {
         final String[] whoClicked = {"P1"};
         final String player1 = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("P1","");
         final String player2 = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("P2","");
-
-
+        TextView playerOne = (TextView) findViewById(R.id.playerOne);
+        TextView playerTwo = (TextView) findViewById(R.id.playerTwo);
+        TextView p1Score = (TextView) findViewById(R.id.p1Score);
+        TextView p2Score = (TextView) findViewById(R.id.p2Score);
+        TextView turn = (TextView) findViewById(R.id.turn);
+        playerOne.setText("Player " + player1 + ":");
+        playerTwo.setText("Player " + player2 + ":");
+        turn.setText(player1 +"\'s\nTurn");
 
         for(int i = 0; i < grid.length; i++){
             int res = getResources().getIdentifier("link"+ (i+1), "id", getPackageName());
@@ -45,7 +57,6 @@ public class GamePage extends AppCompatActivity {
             int res = getResources().getIdentifier("textView"+ (i+1), "id", getPackageName());
             filled[i] = (TextView) findViewById(res);
         }
-
 
         for(int i = 0; i < grid.length; i++){
             if (horizontal.contains(i + 1)) {
@@ -61,15 +72,27 @@ public class GamePage extends AppCompatActivity {
 
                         if (clickCounter[0] % 2 == 0){
                             whoClicked[0] = player1;
+                            turn.setText(player2 +"\'s\nTurn");
+                            turn.setTextColor(Color.parseColor("#D56868"));
                             grid[n].setImageResource(R.drawable.h_blue);
                         }else if (clickCounter[0] % 2 == 1){
                             whoClicked[0] = player2;
+                            turn.setText(player1 +"\'s\nTurn");
+                            turn.setTextColor(Color.parseColor("#73ADCC"));
                             grid[n].setImageResource(R.drawable.h_red);
                         }
 
                         if(topEdges.contains(side)){
                             isFilled = checkTop(n, links);
                             if(isFilled){
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 1:
                                         filled[0].setText(whoClicked[0]);
@@ -85,6 +108,14 @@ public class GamePage extends AppCompatActivity {
                         } else if (bottomEdges.contains(side)) {
                             isFilled = checkBottom(n, links);
                             if(isFilled) {
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 22:
                                         filled[6].setText(whoClicked[0]);
@@ -101,6 +132,14 @@ public class GamePage extends AppCompatActivity {
                         else{
                             isFilled = checkTop(n, links);
                             if(isFilled){
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 8:
                                         filled[3].setText(whoClicked[0]);
@@ -124,6 +163,14 @@ public class GamePage extends AppCompatActivity {
                             }
                             isFilled = checkBottom(n, links);
                             if (isFilled){
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 8:
                                         filled[0].setText(whoClicked[0]);
@@ -162,15 +209,27 @@ public class GamePage extends AppCompatActivity {
 
                         if (clickCounter[0] % 2 == 0){
                             whoClicked[0] = player1;
+                            turn.setText(player2 +"\'s\nTurn");
+                            turn.setTextColor(Color.parseColor("#D56868"));
                             grid[n].setImageResource(R.drawable.v_blue);
                         }else if (clickCounter[0] % 2 == 1){
                             whoClicked[0] = player2;
+                            turn.setText(player1 +"\'s\nTurn");
+                            turn.setTextColor(Color.parseColor("#73ADCC"));
                             grid[n].setImageResource(R.drawable.v_red);
                         }
 
                         if(leftEdges.contains(side)){
                             isFilled = checkLeft(n, links);
                             if(isFilled){
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 4:
                                         filled[0].setText(whoClicked[0]);
@@ -186,6 +245,14 @@ public class GamePage extends AppCompatActivity {
                         } else if (rightEdges.contains(side)) {
                             isFilled = checkRight(n, links);
                             if(isFilled) {
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 7:
                                         filled[2].setText(whoClicked[0]);
@@ -202,6 +269,14 @@ public class GamePage extends AppCompatActivity {
                         else {
                             isFilled = checkLeft(n, links);
                             if (isFilled) {
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 5:
                                         filled[1].setText(whoClicked[0]);
@@ -225,6 +300,14 @@ public class GamePage extends AppCompatActivity {
                             }
                             isFilled = checkRight(n, links);
                             if (isFilled) {
+                                if(whoClicked[0].equalsIgnoreCase(player1)) {
+                                    scores[0]++;
+                                    p1Score.setText(String.valueOf(scores[0]));
+                                }
+                                else {
+                                    scores[1]++;
+                                    p2Score.setText(String.valueOf(scores[1]));
+                                }
                                 switch (side) {
                                     case 5:
                                         filled[0].setText(whoClicked[0]);
@@ -253,7 +336,6 @@ public class GamePage extends AppCompatActivity {
 
         }
     }
-
 
     private boolean checkTop(int side, int[] links){
         boolean isFilled = false;
