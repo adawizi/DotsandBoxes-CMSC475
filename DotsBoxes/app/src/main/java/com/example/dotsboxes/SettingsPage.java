29d71b491688e76b27d6f9796bb1b22b1832a943
adawizi <adawizi@vcu.edu>
2022-03-25 17:11:47 -0400

    Partially implemented color picker

--- DotsBoxes/app/src/main/java/com/example/dotsboxes/SettingsPage.java
+++ DotsBoxes/app/src/main/java/com/example/dotsboxes/SettingsPage.java
@@ -2,13 +2,46 @@ package com.example.dotsboxes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SettingsPage extends AppCompatActivity {
+public class SettingsPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        SharedPreferences.Editor myEditor = myPrefs.edit();
+
+        Spinner spinnerOne = (Spinner) findViewById(R.id.spinnerOne);
+        Spinner spinnerTwo = (Spinner) findViewById(R.id.spinnerTwo);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.color_picker, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOne.setAdapter(adapter);
+        spinnerTwo.setAdapter(adapter);
+
+        spinnerOne.setOnItemSelectedListener(this);
+        spinnerTwo.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        adapterView.getItemAtPosition(i);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
